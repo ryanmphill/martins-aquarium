40cc0ed5bb98093661e90ba1ada063fc0f67b59b
Ryan Phillips <ryanmphill@gmail.com>
2023-04-19 11:52:25 -0500

    Set up structure and design for mobile responsive layout

---
+++ scripts/database.js
@@ -0,0 +1,43 @@
+const database = {
+    fish: [
+        {
+            name: "Donovan",
+            food: "Plankton",
+            size: 18,
+            species: "Bellus Anglefish",
+            origin: "Tahiti",
+            image: "images/bellus_anglefish.jpg"
+        },
+        {
+            name: "Cowboy",
+            food: "Tiny crustaceans",
+            size: 19.8,
+            species: "Dragonface Pipefish",
+            origin: "Vanuatu Islands",
+            image: "images/dragonfacepipefish.jpg"
+        },
+        {
+            name: "Mr. Gills",
+            food: "Tiny crustaceans",
+            size: 50,
+            species: "Formosa Wrasse",
+            origin: "Sri Lanka",
+            image: "images/formosa_wrasse.jpg"
+        },
+        {
+            name: "Vincent",
+            food: "Small crustaceans and other invertebrates",
+            size: 6,
+            species: "Mandarinfish",
+            origin: "RyuKyu Islands",
+            image: "images/mandarinfish.jpg"
+        }
+    ]
+}
+
+/*------------------Export-Data--------------------------------------*/
+/*
+export const getFish = () => {
+    return database.fish.map(fish => ({...fish}))
+}
+*/
